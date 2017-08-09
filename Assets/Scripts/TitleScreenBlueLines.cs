3a01560d7e61d6ed74970056faff1a1942497a85
Meduris <Meduris@users.noreply.github.com>
2017-08-09 22:48:00 +0200

    finished alphachanger script

--- Assets/Scripts/TitleScreenBlueLines.cs
+++ Assets/Scripts/TitleScreenBlueLines.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreenBlueLines : MonoBehaviour {
-	public float alphaChange;
-	private SpriteRenderer rnd;
+	public float alphaChange = -0.003f;
+	public float minAlpha = 0f;
+	public float maxAlpha = 1f;
+
+	private bool invert;
+	private Image img;
 	// Use this for initialization
 	void Start () {
-		rnd = GetComponentInChildren<SpriteRenderer>();
+		img = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rnd.color = new Color(rnd.color.r, rnd.color.g, rnd.color.b, rnd.color.a + alphaChange);
+		if (img.color.a <= minAlpha)
+			invert = true;
+		else if (img.color.a >= maxAlpha)
+			invert = false;
+		float newAlpha = img.color.a + (invert ? -alphaChange : alphaChange);
+		img.color = new Color(img.color.r, img.color.g, img.color.b, newAlpha);
 	}
 }
