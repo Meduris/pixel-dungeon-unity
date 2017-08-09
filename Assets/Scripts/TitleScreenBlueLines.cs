601ca692168c9f8dc0ff92c0023f21074b5358fd
Meduris <Meduris@users.noreply.github.com>
2017-08-09 22:24:16 +0200

    fading for the blue lines in title screen logo

---
+++ Assets/Scripts/TitleScreenBlueLines.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenBlueLines : MonoBehaviour {
+	public float alphaChange;
+	private SpriteRenderer rnd;
+	// Use this for initialization
+	void Start () {
+		rnd = GetComponentInChildren<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rnd.color = new Color(rnd.color.r, rnd.color.g, rnd.color.b, rnd.color.a + alphaChange);
+	}
+}
