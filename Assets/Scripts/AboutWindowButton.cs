615aa1c89f26282ef62820cd60a2d8ac9c7ec189
Meduris <Meduris@users.noreply.github.com>
2017-08-10 21:57:15 +0200

    made the back button and the yellow link on about scene working

---
+++ Assets/Scripts/AboutWindowButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutWindowButton : MonoBehaviour {
+	public GameObject start;
+	public GameObject about;
+
+	public void BackButton()
+	{
+		start.SetActive(true);
+		about.SetActive(false);
+	}
+
+	public void HomePageButton()
+	{
+		Application.OpenURL("http://pixeldungeon.watabou.ru");
+	}
+}
