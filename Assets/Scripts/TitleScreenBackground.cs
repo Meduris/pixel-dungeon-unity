6528239da9b7b1f1b5b233d66f40c6780c049d4d
Meduris <Meduris@users.noreply.github.com>
2017-08-16 22:03:04 +0200

    more changes to the background code to match different resolutions a lot better

--- Assets/Scripts/TitleScreenBackground.cs
+++ Assets/Scripts/TitleScreenBackground.cs
@@ -11,6 +11,8 @@ public class TitleScreenBackground : MonoBehaviour {
 	public GameObject small1;
 	public GameObject small2;
 
+	public float sizeMult = .64f;
+
 	private Vector3 startPos_big1;
 	private Vector3 startPos_big2;
 	private Vector3 startPos_small1;
@@ -18,27 +20,30 @@ public class TitleScreenBackground : MonoBehaviour {
 
 	private Vector2 spriteSize;
 
+	private float worldScreenHeight;
+	private float worldScreenWidth;
+
 	void Start () {
 		startPos_big1 = big1.transform.position;
 		startPos_small1 = small1.transform.position;
-		float camHeight = Camera.main.pixelHeight / 200f;
-		float camWidth = Camera.main.pixelWidth / 200f;
-		spriteSize = new Vector2(camWidth, camHeight);
+		worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+		worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		spriteSize = new Vector2(worldScreenWidth * .32f + .32f, worldScreenHeight * .32f + .32f);
 		big1.GetComponent<SpriteRenderer>().size = spriteSize;
 		small1.GetComponent<SpriteRenderer>().size = spriteSize;
 		big2.GetComponent<SpriteRenderer>().size = spriteSize;
 		small2.GetComponent<SpriteRenderer>().size = spriteSize;
 
-		startPos_big2 = startPos_big1 + (spriteSize.y * 3) * Vector3.down;
-		startPos_small2 = startPos_small1 + (spriteSize.y * 3) * Vector3.down;
+		startPos_big2 = startPos_big1 + (worldScreenHeight + .56f) * Vector3.down;
+		startPos_small2 = startPos_small1 + (worldScreenHeight + .56f) * Vector3.down;
 
 		big2.transform.position = startPos_big2;
 		small2.transform.position = startPos_small2;
 	}
 	
 	void Update () {
-		float newPosSmall = Mathf.Repeat(Time.time * scrollSpeedSmall, spriteSize.y);
-		float newPosBig = Mathf.Repeat(Time.time * scrollSpeedBig, spriteSize.y);
+		float newPosSmall = Mathf.Repeat(Time.time * scrollSpeedSmall, worldScreenHeight + .56f);
+		float newPosBig = Mathf.Repeat(Time.time * scrollSpeedBig, worldScreenHeight + .56f);
 		big1.transform.position = startPos_big1 + newPosBig * Vector3.up;
 		big2.transform.position = startPos_big2 + newPosBig * Vector3.up;
 		small1.transform.position = startPos_small1 + newPosSmall * Vector3.up;
