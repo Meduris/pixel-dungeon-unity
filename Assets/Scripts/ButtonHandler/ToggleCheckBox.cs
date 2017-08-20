f4cff4acbc540025a0ea874aa12e9700a9f3914c
Meduris <Meduris@users.noreply.github.com>
2017-08-20 22:17:28 +0200

    added a script to visually toggle a checkbox when clicking the button which has this script basically switches between two sprites when triggered

---
+++ Assets/Scripts/ButtonHandler/ToggleCheckBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleCheckBox : MonoBehaviour {
+	public Sprite checkedImg;
+	public Sprite uncheckedImg;
+
+	public GameObject checkBox;
+	private bool ticked = true;
+
+	public void toggleCheckBox() {
+		ticked = !ticked;
+		if (ticked)
+			checkBox.GetComponent<Image>().sprite = checkedImg;
+		else
+			checkBox.GetComponent<Image>().sprite = uncheckedImg;
+	}
+}
