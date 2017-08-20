3be7a4abb9d15a529a5cc2ed24f1d62b6cd5f485
Meduris <Meduris@users.noreply.github.com>
2017-08-20 22:15:25 +0200

    set up the soundmanager to have different audio sources for soundFX and music

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,30 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
+	public AudioSource music;
+	public AudioSource soundFX;
+
 	public static bool musicEnabled = true;
 	public static bool soundFXEnabled = true;
 
-	public void enableMusic() {
-		musicEnabled = true;
-	}
+	private SoundManager instance;
 
-	public void disableMusic() {
-		musicEnabled = false;
+	void Awake() {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
 	}
 
 	public void toggleMusic() {
 		musicEnabled = !musicEnabled;
-	}
-
-	public void enableSoundFX() {
-		soundFXEnabled = true;
-	}
-
-	public void disableSoundFX() {
-		soundFXEnabled = false;
+		if (musicEnabled) {
+			music.Play();
+			music.mute = false;
+		} else {
+			music.Stop();
+			music.mute = true;
+		}
 	}
 
 	public void toggleSoundFX() {
 		soundFXEnabled = !soundFXEnabled;
+		if(soundFXEnabled)
+			soundFX.mute = false;
+		else
+			soundFX.mute = true;
+	}
+
+	public void playSoundFX(AudioClip clip) {
+		soundFX.clip = clip;
+		soundFX.Play();
+	}
+
+	public void playMusic(AudioClip clip) {
+		music.Stop();
+		music.clip = clip;
+		music.Play();
 	}
 }
