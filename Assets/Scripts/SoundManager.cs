f85de1f501c58c7fb8936e3cce0faffb0cb0da78
Meduris <Meduris@users.noreply.github.com>
2017-08-20 21:25:57 +0200

    added a SoundManager script with a few basic functions to toggle music and soundeffects of the game

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public static bool musicEnabled = true;
+	public static bool soundFXEnabled = true;
+
+	public void enableMusic() {
+		musicEnabled = true;
+	}
+
+	public void disableMusic() {
+		musicEnabled = false;
+	}
+
+	public void toggleMusic() {
+		musicEnabled = !musicEnabled;
+	}
+
+	public void enableSoundFX() {
+		soundFXEnabled = true;
+	}
+
+	public void disableSoundFX() {
+		soundFXEnabled = false;
+	}
+
+	public void toggleSoundFX() {
+		soundFXEnabled = !soundFXEnabled;
+	}
+}
